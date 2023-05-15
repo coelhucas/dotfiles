e3e1c07c987a6765898ce04956c2f2b009cddb4e
Lucas <lucascoelhodacosta@gmail.com>
2023-05-15 17:45:29 -0300

    Update setup script

--- setup.sh
+++ setup.sh
@@ -6,8 +6,12 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
       		 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 	# install zsh
+        echo "Getting zsh..."
         sudo apt install zsh
 	chsh -s $(which zsh)
+
+        echo "Getting unzip..."
+        sudo apt install unzip
 fi
 
 # TODO: for macOS
@@ -15,11 +19,14 @@ fi
 #    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 
-
-# install zsh | TODO: check if zsh was proper installed
+# install oh-my-zsh | TODO: check if zsh was proper installed
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+echo "Getting fnm..."
+curl -fsSL https://fnm.vercel.app/install | bash
+
 # symlinks
+echo "Applying symlinks..."
 ln -s ~/projects/dotfiles/.config ~
 ln -s ~/projects/dotfiles/zsh/.zshrc ~ 
 
